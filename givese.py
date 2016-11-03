e7f77bfe16d8fbed423f3174ebb1df1acf53565b
dawidlew <dawid.lewandowicz@allegro.pl>
2016-11-03 11:49:34 +0100

    Polowiczna obsługa polskich znaków

--- givese.py
+++ givese.py
@@ -7,21 +7,26 @@ import glob, os
 import os.path
 
 
+
 def general_dict(text):
     # tworzymy słownik z pixelami dla printable
     s1 = {}
     for letter in string.printable:
         if len(s1) == 0:
-            s1[letter] = (int(255/1), int(255/1), int(255/1))
+            s1[letter] = (int(255), int(255), int(255))
         else:
             s1[letter] = (int(255 / (len(s1)/20)), int(255 / (len(s1))*10), int(255 / (len(s1))))
 
     # przypisujemy pixele dla podanego zdania
     s2 = []
     for c in text:
-        for k, p in s1.items():
-            if k == c:
-                s2.append([c, p])
+        if c not in string.printable:
+            s2.append(['?', (255, 255, 255)])
+        else:
+            for k, p in s1.items():
+                if k == c:
+                    s2.append([c, p])
+
     for x in s2:
         print x
 
@@ -33,18 +38,14 @@ def general_dict(text):
     else:
         os.mkdir('pict')
 
-
     # zapełniamy katalog plikami
-    char = 0
+    char = 100
     for value in s2:
         i = Image.new(mode='RGB', size=(20, 20), color=value[1])
-        # czy plik istnieje?
-        if not os.path.isfile(('pict/' + str(value) + '_' + str(char) + '.png')):
-             i.save('pict/' + str(value) + '_' + str(char) + '.png')
-        else:
-            char += 1
-            i.save('pict/' + str(value) + '_' + str(char) + '.png')
 
+        i.save('pict/' + str(char) + '.png')
+
+        char += 1
 
     create_one_file()
 
@@ -61,6 +62,7 @@ def create_one_file():
     new_im = Image.new('RGB', (total_width, max_height))
 
     x_offset = 0
+
     for im in images:
         new_im.paste(im, (x_offset, 0))
         x_offset += im.size[0]
@@ -79,6 +81,6 @@ if __name__ == "__main__":
     #     args.text = raw_input('Please input sentences to change > ')
 
     if not args.text:
-        args.text = 'Sieć Lidl, należąca do koncernu Schwarz (podobnie jak Kaufland) przeprowadziła ostatnio badania marek własnych.'
+        args.text = 'Robisz Refinement w trakcie Planowania?'
 
     general_dict(args.text)
