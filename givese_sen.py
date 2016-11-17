55400d650511a1c61149216be554d17686e2bcff
dawidlew <dawid.lewandowicz@allegro.pl>
2016-11-17 12:07:59 +0100

    New version

---
+++ givese_sen.py
@@ -0,0 +1,93 @@
+# coding=utf-8
+from __future__ import division
+import string
+from PIL import Image, ImageDraw
+import glob, os
+import os.path
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+    render_template, flash, _app_ctx_stack, send_file
+import random
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def my_form():
+    return render_template("my-form.html")
+
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    if text is None:
+        return send_file('none.png')
+    else:
+        general_dict(text)
+        return send_file('pict/test.jpg')
+
+
+def general_dict(text):
+
+    # tworzymy słownik z pixelami dla printable
+    s1 = {}
+    for letter in string.printable:
+        if len(s1) == 0:
+            s1[letter] = (int(255), int(255), int(255))
+        else:
+            s1[letter] = (int(255 / (len(s1)/20)), int(255 / (len(s1))*5), int(255 / (len(s1))))
+
+    new_file(get_pixels_for_sentence(text, s1))
+
+
+def get_pixels_for_sentence(text, s1):
+    # przypisujemy pixele dla podanego zdania
+    s2 = []
+    y = 0
+    for c in text:
+        if c not in string.printable:
+            # usuwanie podójnego wiersza dla dziwnych znakow
+            if y == 0:
+                s2.append(['?', (1, 1, 1)])
+                y = 1
+            else:
+                y = 0
+        else:
+            for k, p in s1.items():
+                if k == c:
+                    s2.append([c, p])
+    return s2
+
+
+def new_file(s2):
+
+    print s2[3][1]
+
+    i = Image.open("g.png")
+
+    photo = i.convert('RGB')
+
+    pixels = photo.load()
+    width, height = photo.size
+
+    last = None
+    last2 = None
+    new_im = Image.new('RGB', (width, height), (255, 255, 255))
+
+    for x in range(width):
+        for y in range(height):
+            if pixels[x, y] != (255, 255, 255):
+                # dla każdego powtarzającego sie RGB w tej samej linii przypisujemy nowe RGB w nowym pliku
+                if last == pixels[x, y]:
+                    if last2 == [x, y][0]:
+                        # wstawianie pixeli z podanego przez użytkownika zdania
+                        new_im.putpixel([x, y], s2[3][1])
+                    last2 = [x, y][0]
+                last = pixels[x, y]
+
+    new_im.save("foo_new.png")
+
+
+if __name__ == '__main__':
+    text = 'Case study. Jak stworzyliśmy landing page, na którym aż 38% użytkowników '
+    # app.run(debug=True, host='0.0.0.0', port=803)
+    general_dict(text)
